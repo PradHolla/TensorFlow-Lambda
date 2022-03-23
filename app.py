d315a7fdaa5e707c96dff89ca0932a04b5eaf2da
Pradhyumna N Holla <pradholla@gmail.com>
2022-03-23 22:08:11 +0530

    Updated App

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
 import json
 import boto3
-import tensorflow as tf
 import numpy as np
 import PIL.Image as Image
 
-s3 = boto3.client('s3')
+import tensorflow as tf
 
-IMG_SIZE = 224
-IMG_SHAPE = (IMG_SIZE, IMG_SIZE, 3)
+IMAGE_WIDTH = 256
+IMAGE_HEIGHT = 256
+
+IMAGE_SHAPE = (IMAGE_WIDTH, IMAGE_HEIGHT)
+model = tf.keras.models.load_model('model/gameClass.h5')
+s3 = boto3.resource('s3')
+
+def lambda_handler(event, context):
+  bucket_name = event['Records'][0]['s3']['bucket']['name']
+  key = event['Records'][0]['s3']['object']['key']
+
+  img = readImageFromBucket(key, bucket_name).resize(IMAGE_SHAPE)
+  img = np.array(img)/255.0
+
+  prediction = model.predict(img[np.newaxis, ...])
+  predicted_class = imagenet_labels[np.argmax(prediction[0], axis=-1)]
+
+  print('ImageName: {0}, Prediction: {1}'.format(key, predicted_class))
+
+def readImageFromBucket(key, bucket_name):
+  bucket = s3.Bucket(bucket_name)
+  object = bucket.Object(key)
+  response = object.get()
+  return Image.open(response['Body'])
